5c4e98c3e7adcbe3ea333c289fa5f7973219ac8e
yhlong0 <yhlong2@gmail.com>
2017-04-05 22:36:28 -0500

    Add EventController and Event Detail page

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,7 +1,40 @@
-var app = angular.module("eventsApp");
+'use strict'
 
-var EventController = function($scope){
-
-};
-
-app.controller("EventController", EventController);
+eventsApp.controller('EventController', 
+	function EventController($scope){
+		
+		$scope.event = {
+			name: 'Angular Boot Camp',
+			date: '1/1/2013',
+			time: '10:30 am',
+			location: {
+				address: '6565 McCallum Blvd',
+				city: 'Dallas',
+				province: 'TX'
+			},
+			imageUrl: '/img/angularjs-logo.png',
+			sessions: [
+				{
+					name: 'Directive Masterclass',
+					creatorName: 'Hailong Yang',
+					duration: '1 hr',
+					level: 'Advanced',
+					abstract: 'In this session you will balabala'
+				},
+				{
+					name: 'Scope for fun',
+					creatorName: 'John Smith',
+					duration: '30 mins',
+					level: 'Advanced',
+					abstract: 'In this session you will balabala'
+				},
+				{
+					name: 'Well behave controller',
+					creatorName: 'test',
+					duration: '2 hours',
+					level: 'Advanced',
+					abstract: 'In this session you will balabala'
+				}
+			]
+		}
+});
